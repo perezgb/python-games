5fa5bc3d08f08375361dc72fbe3c22b0cdbc6838
pguess <pguessbird@gmail.com>
2019-09-13 21:13:11 -0400

    Update guess.py

--- guess_number/guess.py
+++ guess_number/guess.py
@@ -1,15 +1,35 @@
 import random
 
 print('Hello! This is a guess the number game!')
-print('What is your name dude?')
+print('What is your name?')
+
+start = 1
+end = 20
 
 myname=input()
-number = random.randint(1, 104)
-print('hey ' +myname+ ' dude im thinking of a number between 1 and 104 and you have to guess')
+number = random.randint(start, end)
+guessesTaken = 0
 
-guess = input()
-guess =  int(guess)
+print('Hey ' +myname+ ' I am thinking of a number between ' +str(start)+' and ' +str(end))
 
-print('your guess is ' +str(guess))
+for guessesTaken in range(5):
+    print('Take a guess!')
+    guess = input()
+    guess= int(guess)
+ 
+    if guess < number:
+        print('Higher!')
+    
+    if guess > number:
+        print('Lower...')    
 
-print('the answer is ' + str(number))
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print('Congrats! ' +myname+ ', you guessed my number in ' +guessesTaken+ ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Boohoo! Your guesses are over, and the number I was thinking of was' +number+ '.')
